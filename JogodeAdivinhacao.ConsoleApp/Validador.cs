95a3c98ff5a2377c15b3bd7373b856f9e968d938
Agatha Sates <satesagatha@gmail.com>
2025-03-18 00:35:10 -0300

    Classe de validadores criada

---
+++ JogodeAdivinhacao.ConsoleApp/Validador.cs
@@ -0,0 +1,15 @@
+﻿namespace JogodeAdivinhacao.ConsoleApp
+{
+    class Validador
+    {
+        public static int EhUmaOpcao(int min, int max)
+        {
+            int opcao;
+            while (!int.TryParse(Console.ReadLine(), out opcao) || opcao < min || opcao > max)
+            {
+                Escreva.OpcaoInválida();
+            }
+            return opcao;
+        }
+    }
+}
